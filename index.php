006d50480518192a11bff3aa143bb5aa8d52a5ed
MamoruSanCD <105603566+MamoruSanCD@users.noreply.github.com>
2023-08-06 10:25:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style.css">
+    <title>Bem Vindo</title>
+</head>
+<body>
+    <div class="container">
+        <H3>Sejá bem-vindo ao</H3><br/>
+        <h1>Meu Curriculo</h1>
+        <div class="Login">
+            <H2>Login</H2>
+            <form action="login.php" method="post" name="loginform">
+                <br>
+                <br>
+                <input type="email" name="emaillogin" id="text_1">
+                <br>
+                <br>
+                <input type="password" name="pwlogin" id="text_1">
+                <br>
+                <br>
+                <input type="submit" value="loginform">
+            </form>
+        </div>
+        <p>Não tem um login? <a href="registrar.php">Clique aqui!</a></p>
+    </div>
+</body>
+</html>
