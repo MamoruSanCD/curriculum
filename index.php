5381578874a47a437cc2a39e2dc99d98d7ec2967
MamoruSanCD <105603566+MamoruSanCD@users.noreply.github.com>
2023-08-11 14:44:46 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+re<!DOCTYPE html>
 <html lang="pt-br" class="dark">
 <head>
     <meta charset="UTF-8">
@@ -48,37 +48,32 @@
         <div class="pessoal_inform">
             <H3>Matheus Alves de Barros Ribeiro</H3>
             <p>Brasileiro, solteiro, 24 anos</p>
-            <p>Rua Romeu Banafé - Monte Sion</p>
-            <p>Suzano - SP 08613-130</p>
-            <p>telefone/whatsapp: (11) 933221962</p>
-            <p>E-mail: matheusribeiro1045@hotmail.com</p>
+            <p>Endereço sem número (preserve seus dados)</p>
+            <p>(Cidade, Estado, CEP)</p>
+            <p>telefone/whatsapp: </p>
+            <p>E-mail:</p>
         </div>
         <div class="Objetivo">
             <h4>Objetivo</h4>
             <p>Desenvolvimento Front-End</p>
         </div>
         <div class="Formacao">
-            <h4>Analise e Desenvolvimento de Sistemas.</h4>
-            <p>Universidade Mogi das Cruzes, previsão de conclusão em 2024.</p>
+            <h4>Nome do Curso</h4>
+            <p>Universidade , previsão de conclusão em (Ano)</p>
         </div>
         <div class="Experiencia">
             <h4>Experiência Profissional</h4>
-            <h5>2021-Atualmente - Grupo All Net</h5>
-            <p>Cargo: Instrutor Pedagógico</p>
-            <p>Principais atividades: Instruir alunos no desenvolvimento Web (HTML, CSS, JavaScript, PHP e MySQL) e com ferramentas da informática básica</p>
-            <p>Instruir alunos nas práticas de manutenção de computadores e gerenciamento de servidores (Linux e Windows)</p>
-            <p>Manter a organização e manutenção das salas de aula.</p>
+            <h5>Inicio - Fim - Empresa</h5>
+            <p>Cargo: </p>
+            <li>Explique suas experiências</li>
+            <li>Explique suas experiências</li>
+            <li>Explique suas experiências</li>
             <br>
-            <h5>2017-2020 - Mega Byte</h5>
-            <p>Cargo: Instrutor Pedagógico/Coordenador de Curso</p>
-            <p>Principais atividades: Instruir alunos no desenvolvimento Web (HTML, CSS, JavaScript, PHP e MySQL) e com ferramentas da informática básica</p>
-            <p>Instruir alunos nas práticas de manutenção de computadores e gerenciamento de servidores (Linux e Windows)</p>
-            <p>Manter a organização e manutenção das salas de aula.</p>
-            <p>Auxiliar instrutores na elaboração de aulas</p>
-            <p>Auxiliar pais e alunos ao longo do curso</p>
-            <p>Organizar materiais e laboratórios</p>
-            <p>Auxiliar na criação de novos cursos e seus materiais didáticos.</p>
-
+            <h5>Inicio - Fim - Empresa</h5>
+            <p>Cargo: </p>
+            <li>Explique suas experiências</li>
+            <li>Explique suas experiências</li>
+            <li>Explique suas experiências</li>
         </div>
         <h4>Qualificações e Atividades Complementares</h4>
         <li>Espanhol Intermediário</li>
